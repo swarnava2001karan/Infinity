7cc6a0c2dfca7dc016353b38696aff47b491fbcd
Swarnava Karan <swarnavakaran2001@gmail.com>
2024-06-23 21:35:02 +0530

    changes

--- src/components/design/Services.jsx
+++ src/components/design/Services.jsx
@@ -18,7 +18,7 @@ export const Gradient = () => {
 export const PhotoChatMessage = () => {
   return (
     <div className="absolute top-8 right-8 max-w-[17.5rem] py-6 px-8 bg-black rounded-t-xl rounded-bl-xl font-code text-base lg:top-16 lg:right-[8.75rem] lg:max-w-[17.5rem]">
-      Hey Brainwave, enhance this photo
+      Hey Infinity, enhance this photo
       <ChatBubbleWing className="absolute left-full bottom-0" />
     </div>
   );
@@ -37,7 +37,7 @@ export const VideoChatMessage = () => {
         />
       </div>
       <p className="tagline absolute right-2.5 bottom-1 text-[0.625rem] text-n-3 uppercase">
-        just now
+        Created just now
       </p>
       <ChatBubbleWing
         className="absolute right-full bottom-0 -scale-x-100"
